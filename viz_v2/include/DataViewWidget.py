1adf27c8c62dcc6214bd221ac1ac0f756d88ab4f
GabeCasciano <gabecasciano@gmail.com>
2025-09-26 12:56:32 -0400

    Treeview is coming along

--- viz_v2/include/DataViewWidget.py
+++ viz_v2/include/DataViewWidget.py
@@ -1,25 +1,115 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QHBoxLayout, QWidget
+from PyQt5.QtWidgets import QHBoxLayout, QTreeView, QVBoxLayout, QWidget
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
 
 
 class DataViewWidget(QWidget):
 
     class SensorDataTree(QWidget):
-        def __init__(self, parent = None):
+        def __init__(self, parent=None):
             super().__init__(parent)
 
+            _model = QStandardItemModel()
+            _model.setHorizontalHeaderLabels(["Key", "Value"])
+
+            # Common Items
+
+            _ll_k = QStandardItem("LL")
+            _ll_k.setEditable(False)
+
+            _lq_k = QStandardItem("LQ")
+            _lq_k.setEditable(False)
+
+            _rq_k = QStandardItem("RQ")
+            _rq_k.setEditable(False)
+
+            _rr_k = QStandardItem("RR")
+            _rr_k.setEditable(False)
+
+            # Ultrasonic Sensotrs Items
+            _usd_k = QStandardItem("Ultrasonice Sensors")
+            _usd_k.setEditable(False)
+
+            self._usd_ll_v = QStandardItem("0")
+            self._usd_ll_v.setEditable(False)
+
+            self._usd_lq_v = QStandardItem("0")
+            self._usd_lq_v.setEditable(False)
+
+            self._usd_rq_v = QStandardItem("0")
+            self._usd_rq_v.setEditable(False)
+
+            self._usd_rr_v = QStandardItem("0")
+            self._usd_rr_v.setEditable(False)
+
+            _usd_k.appendRow([_ll_k, self._usd_ll_v])
+            _usd_k.appendRow([_lq_k, self._usd_lq_v])
+            _usd_k.appendRow([_rq_k, self._usd_rq_v])
+            _usd_k.appendRow([_rr_k, self._usd_rr_v])
+
+            # Anemometer Sensors Items
+            _anm_k = QStandardItem("Anemometers")
+            _anm_k.setEditable(False)
+
+            self._anm_ll_v = QStandardItem("0")
+            self._anm_ll_v.setEditable(False)
+
+            self._anm_lq_v = QStandardItem("0")
+            self._anm_lq_v.setEditable(False)
+
+            self._anm_rq_v = QStandardItem("0")
+            self._anm_rq_v.setEditable(False)
+
+            self._anm_rr_v = QStandardItem("0")
+            self._anm_rr_v.setEditable(False)
+
+            _anm_k.appendRow([_ll_k.clone(), self._anm_ll_v])
+            _anm_k.appendRow([_lq_k, self._anm_lq_v])
+            _anm_k.appendRow([_rq_k, self._anm_rq_v])
+            _anm_k.appendRow([_rr_k, self._anm_rr_v])
+
+            _model.appendRow(_usd_k)
+            _model.appendRow(_anm_k)
+
+            _tree = QTreeView()
+            _tree.setModel(_model)
+
+            _central_v_box = QVBoxLayout()
+
+            _central_v_box.addWidget(_tree)
+            self.setLayout(_central_v_box)
 
     class SenTeleTree(QWidget):
-        def __init__(self, parent = None):
+        def __init__(self, parent=None):
             super().__init__(parent)
-    
+
+            _model = QStandardItemModel()
+            _model.setHorizontalHeaderLabels(["Key", "Value"])
+
+            _tree = QTreeView()
+            _tree.setModel(_model)
+
+            _central_v_box = QVBoxLayout()
+
+            _central_v_box.addWidget(_tree)
+            self.setLayout(_central_v_box)
+
     class SenConfigTree(QWidget):
-        def __init__(self, parent = None):
+        def __init__(self, parent=None):
             super().__init__(parent)
 
+            _model = QStandardItemModel()
+            _model.setHorizontalHeaderLabels(["Key", "Value"])
 
-    def __init__(self, parent = None):
+            _tree = QTreeView()
+            _tree.setModel(_model)
+
+            _central_v_box = QVBoxLayout()
+
+            _central_v_box.addWidget(_tree)
+            self.setLayout(_central_v_box)
+
+    def __init__(self, parent=None):
         super().__init__(parent)
 
         center_h_box = QHBoxLayout()
@@ -33,5 +123,3 @@ class DataViewWidget(QWidget):
         center_h_box.addWidget(self._sen_config_tree)
 
         self.setLayout(center_h_box)
-
-
