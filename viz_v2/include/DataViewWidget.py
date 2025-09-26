41f8633082a64d56cf79f69ae385b32fea51cbfd
GabeCasciano <gabecasciano@gmail.com>
2025-09-26 13:35:58 -0400

    Sensor dataview is done, with timer

--- viz_v2/include/DataViewWidget.py
+++ viz_v2/include/DataViewWidget.py
@@ -1,75 +1,109 @@
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QTimer, Qt, pyqtSlot
 from PyQt5.QtWidgets import QHBoxLayout, QTreeView, QVBoxLayout, QWidget
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
+
+from .DataStructures import SensorData, SenConfigModel, SenTelemetry
 
 
 class DataViewWidget(QWidget):
 
     class SensorDataTree(QWidget):
-        def __init__(self, parent=None):
+        def __init__(self, parent=None, timer_int:int = 100):
             super().__init__(parent)
 
             _model = QStandardItemModel()
             _model.setHorizontalHeaderLabels(["Key", "Value"])
 
-            # Common Items
 
-            _ll_k = QStandardItem("LL")
-            _ll_k.setEditable(False)
+            self._t_counter = 0
+            self._t_int = timer_int
+            self._timer = QTimer(self)
+            self._timer.setInterval(timer_int)
+            self._timer.timeout.connect(self.UpdateTimer)
 
-            _lq_k = QStandardItem("LQ")
-            _lq_k.setEditable(False)
-
-            _rq_k = QStandardItem("RQ")
-            _rq_k.setEditable(False)
-
-            _rr_k = QStandardItem("RR")
-            _rr_k.setEditable(False)
+            _time_k = QStandardItem("Elapsed from Rx")
+            _time_k.setEditable(False)
+            self._time_v = QStandardItem(str(self._t_counter))
+            self._time_v.setEditable(False)
 
             # Ultrasonic Sensotrs Items
             _usd_k = QStandardItem("Ultrasonice Sensors")
             _usd_k.setEditable(False)
 
+            _usd_ll_k = QStandardItem("LL")
+            _usd_ll_k.setEditable(False)
             self._usd_ll_v = QStandardItem("0")
             self._usd_ll_v.setEditable(False)
 
+            _usd_lq_k = QStandardItem("LQ")
+            _usd_lq_k.setEditable(False)
             self._usd_lq_v = QStandardItem("0")
             self._usd_lq_v.setEditable(False)
 
+            _usd_rq_k = QStandardItem("RQ")
+            _usd_rq_k.setEditable(False)
             self._usd_rq_v = QStandardItem("0")
             self._usd_rq_v.setEditable(False)
 
+            _usd_rr_k = QStandardItem("RR")
+            _usd_rr_k.setEditable(False)
             self._usd_rr_v = QStandardItem("0")
             self._usd_rr_v.setEditable(False)
 
-            _usd_k.appendRow([_ll_k, self._usd_ll_v])
-            _usd_k.appendRow([_lq_k, self._usd_lq_v])
-            _usd_k.appendRow([_rq_k, self._usd_rq_v])
-            _usd_k.appendRow([_rr_k, self._usd_rr_v])
+            _usd_k.appendRow([_usd_ll_k, self._usd_ll_v])
+            _usd_k.appendRow([_usd_lq_k, self._usd_lq_v])
+            _usd_k.appendRow([_usd_rq_k, self._usd_rq_v])
+            _usd_k.appendRow([_usd_rr_k, self._usd_rr_v])
 
             # Anemometer Sensors Items
             _anm_k = QStandardItem("Anemometers")
             _anm_k.setEditable(False)
 
+            _anm_ll_k = QStandardItem("LL")
+            _anm_ll_k.setEditable(False)
             self._anm_ll_v = QStandardItem("0")
             self._anm_ll_v.setEditable(False)
 
+            _anm_lq_k = QStandardItem("LQ")
+            _anm_lq_k.setEditable(False)
             self._anm_lq_v = QStandardItem("0")
             self._anm_lq_v.setEditable(False)
 
+            _anm_rq_k = QStandardItem("RQ")
+            _anm_rq_k.setEditable(False)
             self._anm_rq_v = QStandardItem("0")
             self._anm_rq_v.setEditable(False)
 
+            _anm_rr_k = QStandardItem("RR")
+            _anm_rr_k.setEditable(False)
             self._anm_rr_v = QStandardItem("0")
             self._anm_rr_v.setEditable(False)
 
-            _anm_k.appendRow([_ll_k.clone(), self._anm_ll_v])
-            _anm_k.appendRow([_lq_k, self._anm_lq_v])
-            _anm_k.appendRow([_rq_k, self._anm_rq_v])
-            _anm_k.appendRow([_rr_k, self._anm_rr_v])
+            _anm_k.appendRow([_anm_ll_k, self._anm_ll_v])
+            _anm_k.appendRow([_anm_lq_k, self._anm_lq_v])
+            _anm_k.appendRow([_anm_rq_k, self._anm_rq_v])
+            _anm_k.appendRow([_anm_rr_k, self._anm_rr_v])
 
+            # Standing wave Items
+            _sw_k = QStandardItem("Standing Wave")
+
+            _sw_left_k = QStandardItem("Left")
+            _sw_left_k.setEditable(False)
+            self._sw_left_v = QStandardItem("0")
+            self._sw_left_v.setEditable(False)
+
+            _sw_right_k = QStandardItem("Right")
+            _sw_right_k.setEditable(False)
+            self._sw_right_v = QStandardItem("0")
+            self._sw_right_v.setEditable(False)
+
+            _sw_k.appendRow([_sw_left_k, self._sw_left_v])
+            _sw_k.appendRow([_sw_right_k, self._sw_right_v])
+
+            _model.appendRow([_time_k, self._time_v])
             _model.appendRow(_usd_k)
             _model.appendRow(_anm_k)
+            _model.appendRow(_sw_k)
 
             _tree = QTreeView()
             _tree.setModel(_model)
@@ -78,6 +112,34 @@ class DataViewWidget(QWidget):
 
             _central_v_box.addWidget(_tree)
             self.setLayout(_central_v_box)
+
+            self._timer.start()
+
+        @pyqtSlot()
+        def UpdateTimer(self):
+            self._t_counter += self._t_int
+            self._time_v.setText(f"{self._t_counter} ms")
+
+        @pyqtSlot()
+        def UpdateTree(self, data: SensorData):
+
+            self._timer.stop()
+            self._t_counter = 0
+
+            self._usd_ll_v.setText(str(data.Ultra_Sonic_Distance.LL))
+            self._usd_lq_v.setText(str(data.Ultra_Sonic_Distance.LQ))
+            self._usd_rq_v.setText(str(data.Ultra_Sonic_Distance.RQ))
+            self._usd_rr_v.setText(str(data.Ultra_Sonic_Distance.RR))
+
+            self._anm_ll_v.setText(str(data.Anemometer.LL))
+            self._anm_lq_v.setText(str(data.Anemometer.LQ))
+            self._anm_rq_v.setText(str(data.Anemometer.RQ))
+            self._anm_rr_v.setText(str(data.Anemometer.RR))
+
+            self._sw_left_v.setText(str(data.Standing_Wave.Left))
+            self._sw_right_v.setText(str(data.Standing_Wave.Right))
+
+            self._timer.start()
 
     class SenTeleTree(QWidget):
         def __init__(self, parent=None):
